3631a5b7cd91382e47e06e0d3ef01f7d02fcefe6
majeed4u <jamodah20@gmail.com>
2026-03-14 01:03:57 +0300

    01-Email-Feature-Setup

---
+++ packages/auth/src/lib/mailer.ts
@@ -0,0 +1,32 @@
+import { OTPEmail } from "@client-pulse/ui/components/email-otp";
+import { env } from "@client-pulse/env/server";
+import { render } from "@react-email/components";
+import { Resend } from "resend";
+
+const resend = new Resend(env.RESEND_API_KEY);
+export async function sendOTPEmail(
+  email: string,
+  otp: string,
+  type?: "sign-in" | "email-verification" | "other",
+) {
+  const emailHtml = await render(OTPEmail({ email, otp, type }));
+  const subjectMap = {
+    "sign-in": "Your Sign-In Verification Code",
+    "email-verification": "Verify Your Email Address",
+    other: "Your One-Time Password (OTP)",
+  };
+
+  const { data, error } = await resend.emails.send({
+    from: `${env.RESEND_FROM_NAME} <${env.RESEND_FROM_EMAIL}>`,
+    to: [email],
+    subject: subjectMap[type || "other"],
+    html: emailHtml,
+  });
+
+  if (error) {
+    console.error("Failed to send OTP email:", error);
+    throw new Error("Failed to send OTP email");
+  }
+
+  return data;
+}
