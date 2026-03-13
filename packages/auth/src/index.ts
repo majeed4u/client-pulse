b73b04c8511bf2451f77d00f9537c66b80fa7bc9
majeed4u <jamodah20@gmail.com>
2026-03-13 21:46:52 +0300

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,25 @@
+import prisma from "@client-pulse/db";
+import { env } from "@client-pulse/env/server";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
